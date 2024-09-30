992668b3908386124170f6137394a29badf5e316
h1tmd <105845103+h1tmd@users.noreply.github.com>
2024-09-30 12:50:53 +0800

    Add y-sorting to sprites

--- player.gd
+++ player.gd
@@ -4,10 +4,15 @@ extends CharacterBody2D
 @export var acceleration = 5
 var direction = Vector2.ZERO
 @onready var interact_reach: Area2D = $InteractReach
+@onready var sprite_2d = $Sprite2D
 
-func _process(delta):
+func _process(_delta):
 	direction = Input.get_vector("left","right","up","down")
 	interact_reach.look_at(global_position + direction)
+	if direction.x > 0:
+		sprite_2d.flip_h = true
+	elif direction.x < 0:
+		sprite_2d.flip_h = false
 
 func _physics_process(delta):
 	if direction.length() > 0:
