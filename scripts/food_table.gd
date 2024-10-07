58f1606445cf74e4fb378c6bbe02f26cac04a4ee
h1tmd <105845103+h1tmd@users.noreply.github.com>
2024-10-07 17:19:46 +0800

    Outline shader test

--- scripts/food_table.gd
+++ scripts/food_table.gd
@@ -1,14 +1,17 @@
 extends StaticBody2D
 
 @onready var serve_ui = $"CanvasLayer/Serve UI"
+@onready var sprite_2d = $Sprite2D
 var can_interact = false
 
 func _on_area_2d_area_entered(area):
 	if area.name == "InteractReach":
+		sprite_2d.material.set_shader_parameter("line_thickness", 8)
 		can_interact = true
 
 func _on_area_2d_area_exited(area: Area2D):
 	if area.name == "InteractReach":
+		sprite_2d.material.set_shader_parameter("line_thickness", 0)
 		can_interact = false
 		serve_ui.hide()
 
