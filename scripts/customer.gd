c08c45932a643fd806e7ca14582d06f7e3812990
h1tmd <105845103+h1tmd@users.noreply.github.com>
2024-10-07 11:14:01 +0800

    Remove print statements

--- scripts/customer.gd
+++ scripts/customer.gd
@@ -116,7 +116,6 @@ func _process(delta: float) -> void:
 
 func _on_person_radar_body_entered(body: Node2D) -> void:
 	person_in_front = check_front()
-	print(check_front())
 
 func _on_person_radar_body_exited(body: Node2D) -> void:
 	person_in_front = check_front()
@@ -124,7 +123,7 @@ func _on_person_radar_body_exited(body: Node2D) -> void:
 
 func check_front() -> bool:
 	var bodies = person_radar.get_overlapping_bodies()
-	print(bodies)
+
 	for body in bodies:
 		# Stop if person in front is a customer that has not yet ordered
 		if not done_eating and body is Customer and body != self:
