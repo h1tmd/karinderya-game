3a25a2636e6eb44703815a3d322c79f9600df4f0
h1tmd <105845103+h1tmd@users.noreply.github.com>
2024-10-10 14:39:33 +0800

    Change outline color, code cleanup

--- scripts/dish_serving.gd
+++ scripts/dish_serving.gd
@@ -2,11 +2,17 @@ extends Sprite2D
 class_name DishServing
 
 @onready var area_2d: Area2D = $Area2D
+@onready var collision_shape_2d: CollisionShape2D = $Area2D/CollisionShape2D
+@onready var rice_collision: CollisionShape2D = $"Area2D/Rice Collision"
+
 var mouse_offset = Vector2.ZERO
 var area_name = "Serving Area"
 var mouse_over = false
 var selected = true
 var dish_data: Dish
+
+const red_color = Color(0.93, 0.32, 0.32, 0.918)
+const yellow_color = Color(1, 0.982, 0.555, 0.918)
 
 
 func set_data(data: Dish):
@@ -18,15 +24,22 @@ func _ready():
 		texture = dish_data.image
 		if dish_data.name == "Rice":
 			area_name = "Plate Area"
+			collision_shape_2d.disabled = true
+			rice_collision.disabled = false
 	selected = true
-	modulate.a = 0.5
+	Global.currently_selected = self
 	scale = Vector2(0.5, 0.5)
+	material.set_shader_parameter("line_color", red_color)
+	material.set_shader_parameter("line_thickness", 15)
 
 
-func _process(_delta):
+func _process(delta):
 	if selected:
-		global_position = get_global_mouse_position() + mouse_offset
-
+		global_position = lerp(
+			global_position, 
+			get_global_mouse_position() + mouse_offset, 
+			delta / 0.1
+		)
 
 func _unhandled_input(_event: InputEvent) -> void:
 	if Input.is_action_just_pressed("click") and mouse_over:
@@ -49,22 +62,24 @@ func _input(_event: InputEvent) -> void:
 
 
 func _on_area_2d_mouse_entered() -> void:
-	mouse_over = true
-	material.set_shader_parameter("line_thickness", 15)
+	if Global.currently_selected == null:
+		mouse_over = true
+		material.set_shader_parameter("line_thickness", 15)
 
 
 func _on_area_2d_mouse_exited() -> void:
 	mouse_over = false
-	material.set_shader_parameter("line_thickness", 0)
+	if not selected:
+		material.set_shader_parameter("line_thickness", 0)
 
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	if area.name == area_name and selected:
-		modulate.a = 1
 		scale = Vector2(0.6, 0.6)
+		material.set_shader_parameter("line_color", yellow_color)
 
 
 func _on_area_2d_area_exited(area: Area2D) -> void:
 	if area.name == area_name and selected:
-		modulate.a = 0.5
 		scale = Vector2(0.5, 0.5)
+		material.set_shader_parameter("line_color", red_color)
