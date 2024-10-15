f6194b5a16d56be6be293c61c94427db37cdace4
h1tmd <105845103+h1tmd@users.noreply.github.com>
2024-10-15 23:45:41 +0800

    Fix to increasing interval

--- scripts/main.gd
+++ scripts/main.gd
@@ -8,13 +8,13 @@ func _ready() -> void:
 		var game_time = (timer.wait_time - timer.time_left) / timer.wait_time
 		var customer_interval
 		if game_time < 0.30:
-			customer_interval = 8
-		elif game_time < 0.60:
-			customer_interval = 10
-		elif game_time < 0.80:
-			customer_interval = 12
-		else:
 			customer_interval = 16
+		elif game_time < 0.60:
+			customer_interval = 12
+		elif game_time < 0.80:
+			customer_interval = 10
+		else:
+			customer_interval = 8
 		await get_tree().create_timer(customer_interval).timeout
 		var cust := customer.instantiate()
 		add_child(cust)
