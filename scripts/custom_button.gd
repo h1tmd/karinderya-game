b663517009dfbb42d9ba6e31b6eebbb8bb49ddfe
h1tmd <105845103+h1tmd@users.noreply.github.com>
2024-10-21 15:51:49 +0800

    Add difficulty selection buttons in the main menu

--- scripts/custom_button.gd
+++ scripts/custom_button.gd
@@ -1,15 +1,40 @@
 extends TextureButton
 
 @onready var center_container: CenterContainer = $CenterContainer
-@onready var label: Label = $CenterContainer/Label
+@onready var label: Label = $CenterContainer/HBoxContainer/Label
+@onready var texture_rect: TextureRect = $CenterContainer/HBoxContainer/TextureRect
 
 @export var label_text: String = "Serve"
+@export var icon: Texture2D
+
+var is_toggled = false
 
 func _ready() -> void:
 	label.text = label_text
+	if icon:
+		texture_rect.texture = icon
+	if button_pressed:
+		move_down()
 
 func _on_button_down() -> void:
-	center_container.position.y = 20
+	if is_toggled:
+		move_up()
+	else:
+		move_down()
 
 func _on_button_up() -> void:
+	if is_toggled:
+		move_down()
+	else:
+		move_up()
+
+func _on_toggled(toggled_on: bool) -> void:
+	is_toggled = toggled_on
+	if not is_toggled:
+		move_up()
+
+func move_down():
+	center_container.position.y = 20
+
+func move_up():
 	center_container.position.y = 0
--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,5 +1,18 @@
 extends Control
 
+@export var diff_buttons: ButtonGroup
+
+
 func _on_custom_button_pressed() -> void:
+	var selected = diff_buttons.get_pressed_button().name
+	match selected:
+		"Comfy":
+			GameState.current_difficulty = Global.diff[0]
+		"Normal":
+			GameState.current_difficulty = Global.diff[1]
+		"Hard":
+			GameState.current_difficulty = Global.diff[2]
+		_:
+			print("Error button name does not match difficulty")
 	get_tree().paused = false
 	hide()
