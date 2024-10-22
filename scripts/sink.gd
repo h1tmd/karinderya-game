ffcc7c6be0202dd900295ee45f43535a9cdb1383
h1tmd <105845103+h1tmd@users.noreply.github.com>
2024-10-23 00:25:55 +0800

    Add click interaction for sink

--- scripts/sink.gd
+++ scripts/sink.gd
@@ -16,41 +16,41 @@ var can_interact = false
 var plates: int = 0
 var washing = false
 
-func _on_area_2d_area_entered(area):
-	if area.name == "InteractReach":
-		sprite_2d.material.set_shader_parameter("line_thickness", 15)
-		can_interact = true
-
-func _on_area_2d_area_exited(area):
-	if area.name == "InteractReach":
-		sprite_2d.material.set_shader_parameter("line_thickness", 0)
-		can_interact = false
-		timer_circle.hide()
+const WASH_LOCATION = Vector2(1788, 320)
 
 func _unhandled_input(event):
-	if Input.is_action_just_pressed("interact") and can_interact:
-		var player : Player = area_2d.get_overlapping_areas()[0].get_parent()
-		if player.plate_holder.get_child_count() != 0:
-			var plates_recieved = player.plate_holder.get_child_count()
-			for child in player.plate_holder.get_children():
-				child.queue_free()
-			print("Put plates: ", plates_recieved)
-			plates += plates_recieved
-			sfx_receive_plates.play()
-			sprite_2d.texture = sink_dishes_sprite
-			interacted.emit()
-		elif not washing and plates != 0:
-			washing = true
+	if Input.is_action_just_pressed("click") and can_interact:
+		Global.player.go_to(WASH_LOCATION)
+		await Global.player.arrived
+		print(Global.player.position, WASH_LOCATION, Global.player.position == WASH_LOCATION)
+		if Global.player.position == WASH_LOCATION:
+			if Global.player.plate_holder.get_child_count() != 0:
+				var plates_recieved = Global.player.plate_holder.get_child_count()
+				for child in Global.player.plate_holder.get_children():
+					child.queue_free()
+				print("Put plates: ", plates_recieved)
+				plates += plates_recieved
+				sfx_receive_plates.play()
+				sprite_2d.texture = sink_dishes_sprite
+				interacted.emit()
+			
 			timer.start(plates * GameState.current_difficulty["wash_time"])
 			timer_circle.show()
-			while can_interact and plates != 0:
+			while Global.player.position == WASH_LOCATION and plates != 0:
 				await get_tree().create_timer(GameState.current_difficulty["wash_time"]).timeout
 				plates -= 1
 				GameState.available_plates += 1
 				sfx_wash.play()
 				print("Washed a plate.")
 			timer_circle.hide()
-			washing = false
 			if plates == 0:
 				sprite_2d.texture = sink_sprite
 			print("Total plates: " + str(GameState.available_plates))
+
+func _on_area_2d_mouse_entered() -> void:
+	sprite_2d.material.set_shader_parameter("line_thickness", 15)
+	can_interact = true
+
+func _on_area_2d_mouse_exited() -> void:
+	sprite_2d.material.set_shader_parameter("line_thickness", 0)
+	can_interact = false
